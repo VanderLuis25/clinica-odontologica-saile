773f11040cf5da323cbe6ff255a0956066286546
VanderLuis25 <vanderluis2021@outlook.com>
2025-10-04 01:47:59 -0300

    backend atualizado

--- backend/routes/clinicas.js
+++ backend/routes/clinicas.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import Clinica from '../models/Clinica.js';
-import Usuario from '../models/Usuario.js';
+import Usuario from '../models/User.js';
 
 const router = express.Router();
 
